97973074b2a3442b9a536801d56c3385bf8483b1
bahodirovabubakr <bahodirovabubakr117@gmail.com>
2026-03-03 11:47:06 +0500

    code yangilandi

--- Javascript3/lesson.1/index.js
+++ Javascript3/lesson.1/index.js
@@ -27,17 +27,19 @@
 // let arr2 = [1, 2, 3, 4, 5];
 // let arr = { ...arr1, ...arr2 }
 // console.log(arr);
-let sum1 = 0;
-let sum2 = 0;
-let arr1 = [4, 7, 5, 1];
-let arr2 = [10, 20, 15];
-let arr = [...arr1, ...arr2];
-function javob(a, b, ...arr){
-    let sum1 = a+b
-    let sum2 = arr.reduce((c, d) => c+d, 0)
-    console.log(sum2/sum1);
+// let sum1 = 0;
+// let sum2 = 0;
+// let arr1 = [4, 7, 5, 1];
+// let arr2 = [10, 20, 15];
+// let arr = [...arr1, ...arr2];
+// function javob(a, b, ...arr){
+//     let sum1 = a+b
+//     let sum2 = arr.reduce((c, d) => c+d, 0)
+//     console.log(sum2/sum1);
     
-}
+// }
 
-javob(...arr);
+// javob(...arr);
 
+
+console.log("Abubakr");
